97bbc5db201c79aff07dcfe0eac66bc993e336e4
Bailey <nattapolprom02@ggmail.com>
2023-05-20 00:04:31 +0700

    Done

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program {
+    public static void Main()
+    {
+
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+
+        int[] array = new int[N];
+        string[] arrayInput = Console.ReadLine().Split(' ');
+
+        for(int i = 0; i < N; i++){
+            array[i] = int.Parse(arrayInput[1]);
+        }
+        int max = 0;
+        int flag;
+        for (int i = K; i < N - K; i++){
+            flag = array[i];
+            for (int j = 1;j < K + 1;j++){
+                flag += array[i - j];
+                flag += array[i + j];
+            }
+            if (flag > max){
+                max = flag;
+            }
+        }
+        Console.WriteLine(max);
+    }
+}
